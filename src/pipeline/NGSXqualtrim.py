2eea6ea734be07efdf1388e529a0956910c28a5b
Katherine Eaton <ktmeaton@gmail.com>
2016-03-02 16:21:23 -0500

    Update NGSXqualtrim.py

--- src/pipeline/NGSXqualtrim.py
+++ src/pipeline/NGSXqualtrim.py
@@ -57,6 +57,19 @@ mandatoryArgs.add_argument('-l','--length',
                     help='Minimum length of read to keep.',
                     required=True)
 
+# Read type (pe or se)
+mandatoryArgs.add_argument('-r','--read-type',
+		    choices=["se","pe"]
+                    dest='read_type',
+                    help='Read type (single-end or paired-end).',
+                    required=True)     
+
+# Input suffix
+mandatoryArgs.add_argument('-s','-input-suffix',
+                    dest='input_suffix',
+                    help='Suffix of input read files.',
+                    required=True)                      
+
 
 args = parser.parse_args()                                                      # Store command line arguments
 
@@ -65,14 +78,8 @@ args = parser.parse_args()                                                      
 #                                  Variables                                    #
 #-------------------------------------------------------------------------------#
 
-
 # An attempt at cross-platform support, TO BE CHANGED IF USING WINDOWS
 OS_SEP = "/"
-
-# Remove duplicate binary
-QUALTRIM = 'bin/NGSXqualtrim'
-
-
 
 samples_dir = args.samples_directory                                            # Directory of sample directories
 output_dir = args.output_directory                                              # Directory for output files
@@ -80,12 +87,29 @@ phred_base = args.phred_base
 min_qual = args.min_qual
 prop = args.proportion
 min_length = args.min_length
-
+read_type = args.read_type
+input_suffix = args.input_suffix
 
 # Safety Dir
 safety_dir = output_dir + OS_SEP + 'safety'
 if not os.path.exists(safety_dir):
     os.makedirs(safety_dir)
+
+# Script path
+script_path = os.path.realpath(__file__)					# Retrieve the path of the NGSXclassify.py file
+split_path = script_path.split(OS_SEP)						# Split by operating system delimieter
+NGSX_root_list = split_path[:len(split_path)-3]					# Jump up 2 levels to root directory
+NGSX_root_path = ""								# Create the string of the NGSX root path
+for directory in NGSX_root_list:
+    NGSX_root_path += OS_SEP + directory
+    
+# Read type binary choice
+if read_type == "se":
+    QUALTRIM = NGSX_root_path + OS_SEP + 'bin' + OS_SEP + 'NGSXQualityControl'
+elif read_type == "pe":
+    QUALTRIM = NGSX_root_path + OS_SEP + 'bin' + OS_SEP + 'NGSXQualityControlPairedEnd'
+else:
+    raise ValueError("User-specified read type was not 'pe' or 'se'.")	
 
 
 makefile = open('NGSXqualtrim.mk', 'w')                                        #Create the makefile
@@ -124,7 +148,7 @@ print(TextColor.BLUE + 'Creating commands to run quality and length filtering on
 for removedup_ind_sample_dir in os.listdir(samples_dir):
     removedup_ind_sample_path = samples_dir + OS_SEP + removedup_ind_sample_dir
     for removedup_file in os.listdir(removedup_ind_sample_path):
-        if removedup_file.endswith('.sort.fastq'):
+        if removedup_file.endswith(input_suffix):
             removedup_file_path = removedup_ind_sample_path + OS_SEP + removedup_file
             print (TextColor.RED + removedup_file_path)
 
@@ -139,21 +163,18 @@ for removedup_ind_sample_dir in os.listdir(samples_dir):
                                 removedup_file.replace('.fastq','.qualtrim.fastq'))
             all_target_list.append(qualtrim_target)
 
-            # Reject fastq file
-            qualtrim_reject = (output_dir_qual + OS_SEP +
-                                removedup_file.replace('.fastq','.qualtrim.reject.fastq'))
-
             #Stats file for percent unique
             stats_file = qualtrim_target.replace('.fastq','.stats')
 
-            # Write qualtrim commands to makefile for execution
+            # Write qualtrim targets
             makefile.write(qualtrim_target + ': ' + removedup_file_path + '\n')
 
             # Write the echo qualtrim statement for shell output
             makefile.write('\t' + "@echo -e '\e[31m" +
                     'Quality and length filtering for sample: ' + removedup_file.split('.')[0] +
                     '\e[0m' + "'" + '\n')
-
+	    
+	    # Write qualtrim commands to makefile for execution
             makefile.write('\t' + '@' + QUALTRIM + ' ' +
                         str(phred_base) + ' ' +
                         str(min_qual) + ' ' +
@@ -164,14 +185,10 @@ for removedup_ind_sample_dir in os.listdir(samples_dir):
                         qualtrim_reject + ' ' +
                         stats_file + '\n')
 
-
+	    # Write command to transfer stats file to the safety directory
    	    makefile.write('\t' + '@cp ' +
 			stats_file + ' ' +
 			safety_dir + '\n\n')
-
-
-
-
 
 
 
