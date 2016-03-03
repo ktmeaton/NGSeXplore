c77d29f97f38c3f1ced7bf1305e8b506da63af4f
Katherine Eaton <ktmeaton@gmail.com>
2016-03-02 19:47:39 -0500

    Update NGSXQualityControlPairedEnd.cpp

--- src/modules/NGSXQualityControlPairedEnd.cpp
+++ src/modules/NGSXQualityControlPairedEnd.cpp
@@ -3,41 +3,45 @@
  *  \verbinclude NGSXQualityControlPairedEnd.cpp
  *
  * NGSXQualityControlPairedEnd: Implementation
- * Date: 2016-0222
- * \author $Author : Katherine Eaton
+ *\date Date: 2016-0222
+ *
+ * Date Edited: 2016-0302 -	Added proper command-line parsing
+ *
+ *\author Author : Katherine Eaton
+ *
  * Contact: ktmeaton [at sign here ] gmail.com
  *
  */
 
 //----------------------------System Include----------------------------------//
-#include <iostream>									// Input and output to screen
-#include <string>										// String
-#include <iomanip>									// Set Precision
-#include <fstream>									// File input and output
-#include <sstream>									// Argument to int
+#include <iostream>								// Input and output to screen
+#include <string>								// String
+#include <iomanip>								// Set Precision
+#include <fstream>								// File input and output
+#include <sstream>								// Argument to int
 #include <algorithm>								// Count funtion
 
 //----------------------------Custom Include----------------------------------//
-#include "TextColor.h"							// Unix shell colored output
-#include "ProgressLog.h"						// ProgressLog Class
-#include "Patch.cpp"                // Patch to fix the int to string compiler issue
+#include "TextColor.h"								// Unix shell colored output
+#include "ProgressLog.h"							// ProgressLog Class
+#include "Patch.cpp"            						// Patch to fix the int to string compiler issue
 
 //---------------------------------Main---------------------------------------//
 int main(int argc, char* argv[])
 {
-	//---------------------------Help Variables---------------------------------//
+	//---------------------------Help Variables---------------------------//
 	std::string usage = std::string("NGSX Quality Control Paired End Module. Filters by quality threshold for paired-end reads, removes reads with N's and short reads. \n") +
-									"Usage:\n" +
-										"\t" +
-										std::string(argv[0]) +
-										" [--phred] [-q] [-p] [-l] [input fastq 1 file] [input fastq 2 file] [output fastq 1 file] [output fastq 2 file] [stats file]\n\n" +
-									"Options:\n" +
-										"\t" +
-										"[--phred BASE]\t\t= 33 or 64\n" +
-										"\t" +
-										"[-q INT]\t\t= Minimum quality threshold\n" +
-										"\t[-p FLOAT (0-1)]\t= Percentage of read that must meet minimum quality threshold (DEFAULT: 0.5)\n" +
-										"\t[-l INT]\t\t= Minimum length to keep";
+					"Usage:\n" +
+					"\t" +
+					std::string(argv[0]) +
+					" [--phred] [-q] [-p] [-l] [input fastq 1 file] [input fastq 2 file] [output fastq 1 file] [output fastq 2 file] [stats file]\n\n" +
+					"Options:\n" +
+					"\t" +
+					"[--phred BASE]\t\t= 33 or 64\n" +
+					"\t" +
+					"[-q INT]\t\t= Minimum quality threshold\n" +
+					"\t[-p FLOAT (0-1)]\t= Percentage of read that must meet minimum quality threshold (DEFAULT: 0.5)\n" +
+					"\t[-l INT]\t\t= Minimum length to keep";
 
 	//-----------------------------Help Message---------------------------------//
 	if ((argc == 1) ||
@@ -53,67 +57,91 @@ int main(int argc, char* argv[])
 
 	//----------------------------Variables: FILE-------------------------------//
 	std::string fastq_1_file_name;
-	fastq_1_file_name = argv[5];																						// Argument 5: Name of the input fastq 1 file
+	fastq_1_file_name = argv[5];							// Argument 5: Name of the input fastq 1 file
 
 	std::string fastq_2_file_name;
-	fastq_2_file_name = argv[6];																						// Argument 6: Name of the input fastq 2 file
+	fastq_2_file_name = argv[6];							// Argument 6: Name of the input fastq 2 file
 
 	std::string filter_fastq_1_file_name;
-	filter_fastq_1_file_name = argv[7];																			// Argument 7: Name of the output unique fastq 1 file
+	filter_fastq_1_file_name = argv[7];						// Argument 7: Name of the output unique fastq 1 file
 
 	std::string filter_fastq_2_file_name;
-	filter_fastq_2_file_name = argv[8];																			// Argument 8: Name of the output unique fastq 2 file
+	filter_fastq_2_file_name = argv[8];						// Argument 8: Name of the output unique fastq 2 file
 
 	std::string stats_file_name;
-	stats_file_name = argv[9];																							// Argument 9: Name of the stats file
+	stats_file_name = argv[9];							// Argument 9: Name of the stats file
 
 
-	std::ifstream fastq_1_file;																							// Creates an input file stream for the input fastq 1 file
-	std::ifstream fastq_1_file_copy;																				// Copy for counting the number of lines
+	std::ifstream fastq_1_file;							// Creates an input file stream for the input fastq 1 file
+	std::ifstream fastq_1_file_copy;						// Copy for counting the number of lines
 
-	std::ifstream fastq_2_file;																							// Creates an input file stream for the input fastq 2 file
+	std::ifstream fastq_2_file;							// Creates an input file stream for the input fastq 2 file
 
-	std::ofstream filter_fastq_1_file;																			// Creates an output file stream for the filtered output fastq 1 file
-	std::ofstream filter_fastq_2_file;																			// Creates an output file stream for the filtered output fastq 2 file
-	std::ofstream stats_file;													 	 										// Creates an output file stream for the stats file
+	std::ofstream filter_fastq_1_file;						// Creates an output file stream for the filtered output fastq 1 file
+	std::ofstream filter_fastq_2_file;						// Creates an output file stream for the filtered output fastq 2 file
+	std::ofstream stats_file;							// Creates an output file stream for the stats file
 
+	
+	std::string parameter;								// String to store current command-line argument
+	std::string str_phred;								// Temp string to store input phred base
+	std::string str_min_qual;							// Temp string to store input minimum quality
+	std::string str_prop_thresh;							// Temp string to store input proportion threshold
+	std::string str_min_len;							// Temp string to store input minimum length
+	
+	
+	//---------------------Variables: ARGUMENT PARSING---------------------------//
+	for(int i=1; i<(argc-5); i++) // Look through all command-line arguments except last 3
+	{ 
+	    parameter = argv[i];
+	    
+	    if(parameter == "--phred") {str_phred = argv[i+1]; continue;}
+	    else if(parameter == "-q") {str_min_qual = argv[i+1]; continue;}
+	    else if(parameter == "-p") {str_prop_thresh = argv[i+1]; continue;}
+	    else if(parameter == "-l") {str_min_len = argv[i+1]; continue;}
+	    else std::cerr<<"Unknown argument "<< argv[i] <<" exiting"<< std::endl;
+	    return 1;	
+	}
 
-	//----------------------------Variables: CONSTANT---------------------------//
-	std::istringstream ss_phred(argv[1]);
-  int i_phred;
-  if (!(ss_phred >> i_phred))  std::cerr << "Invalid phred base. " << argv[1] << '\n';
-	const int PHRED_BASE = i_phred;						// Phred base quality
+	//----------------------------Variables: CONSTANT----------------------------//
+	
+	// Phred Base
+	std::istringstream ss_phred(str_phred);
+  	int i_phred;
+  	if (!(ss_phred >> i_phred)) std::cerr << "Invalid phred base. " << str_phred << std::endl;
+	const int PHRED_BASE = i_phred;	
+	
+	// Minimum Quality
+  	std::istringstream ss_min_qual(str_min_qual);
+  	int i_min_qual;
+  	if (!(ss_min_qual >> i_min_qual))  std::cerr << "Invalid minimum quality " << str_min_qual << '\n';
+  	const int MIN_QUAL = i_min_qual;
 
-  std::istringstream ss_min_qual(argv[2]);
-  int i_min_qual;
-  if (!(ss_min_qual >> i_min_qual))  std::cerr << "Invalid minimum quality " << argv[2] << '\n';
-  const int MIN_QUAL = i_min_qual;
+        // Proportion Threshold
+  	std::istringstream ss_prop_thresh(str_prop_thresh);
+  	float f_prop_thresh;
+  	if (!(ss_prop_thresh >> f_prop_thresh))  std::cerr << "Invalid proportion threshold " << str_prop_thresh << '\n';
+	const float PROP_THRESHOLD = f_prop_thresh;					// Proportion of reads that must meet minimum quality threshold
 
-  std::istringstream ss_prop_thresh(argv[3]);
-  float f_prop_thresh;
-  if (!(ss_prop_thresh >> f_prop_thresh))  std::cerr << "Invalid proportion threshold " << argv[3] << '\n';
-	const float PROP_THRESHOLD = f_prop_thresh;				// Proportion of reads that must meet minimum quality threshold
-
-
-  std::istringstream ss_min_len(argv[4]);
-  int i_min_len;
-  if (!(ss_min_len >> i_min_len))  std::cerr << "Invalid minimum length. " << argv[4] << '\n';
-  const int MIN_LENGTH = i_min_len;
+	// Minimum Length
+  	std::istringstream ss_min_len(str_min_len);
+  	int i_min_len;
+  	if (!(ss_min_len >> i_min_len))  std::cerr << "Invalid minimum length. " << str_min_len << '\n';
+  	const int MIN_LENGTH = i_min_len;
 
 	//---------------------------Variables: STAT--------------------------------//
-	int total_num_lines;																										// Number of lines in the copy fastq file
-	int total_num_records;																									// Number of sequences in the copy fastq file
-	int final_num_seq;																											// Number of sequences in filtered fastq file
-	float percent_filtered;																									// Percent of sequences filtered
+	int total_num_lines;								// Number of lines in the copy fastq file
+	int total_num_records;								// Number of sequences in the copy fastq file
+	int final_num_seq;								// Number of sequences in filtered fastq file
+	float percent_filtered;								// Percent of sequences filtered
 
 	//-----------------------Variables: PROCESSSING-----------------------------//
 	// Current line of file
-	std::string current_line_1;																							// String to hold the line read in from the fastq 1 file
-	std::string current_line_2;																							// String to hold the line read in from the fastq 2 file
+	std::string current_line_1;							// String to hold the line read in from the fastq 1 file
+	std::string current_line_2;							// String to hold the line read in from the fastq 2 file
 
 	// Colored text and progress log
-	TextColor::TextColor Palette;																						// TextColor object for coloring text output
-	ProgressLog::ProgressLog fastq_progress_log;														// ProgressLog object to store file processing progress.
+	TextColor::TextColor Palette;							// TextColor object for coloring text output
+	ProgressLog::ProgressLog fastq_progress_log;					// ProgressLog object to store file processing progress.
 
 	// Temporary id holder
 	std::string temp_id_1;
@@ -130,27 +158,27 @@ int main(int argc, char* argv[])
 	int read_length_1;
 	int read_length_2;
 
-	int bases_above_threshold_1;																						// Number of bases in current read that are above quality threshold
-	int bases_above_threshold_2;																						// Number of bases in current read that are above quality threshold
+	int bases_above_threshold_1;							// Number of bases in current read that are above quality threshold
+	int bases_above_threshold_2;							// Number of bases in current read that are above quality threshold
 
-	int base_quality_1;																											// Quality of the current base
-	int base_quality_2;																											// Quality of the current base
+	int base_quality_1;								// Quality of the current base
+	int base_quality_2;								// Quality of the current base
 
-	bool found_N = false;						   																			// boolean to indicate whether an N was found.
+	bool found_N = false;						   		// boolean to indicate whether an N was found.
 	bool reject_read = true;
 
 
-	//----------------------------------Open Files---------------------------------------//
+	//----------------------------------Open Files-------------------------------//
 	// open requires parameter to be a const char*
-	fastq_1_file.open(fastq_1_file_name.c_str());														// Open input fastq 1 file
-	fastq_2_file.open(fastq_2_file_name.c_str());														// Open input fastq 2 file
+	fastq_1_file.open(fastq_1_file_name.c_str());					// Open input fastq 1 file
+	fastq_2_file.open(fastq_2_file_name.c_str());					// Open input fastq 2 file
 
-	fastq_1_file_copy.open(fastq_1_file_name.c_str());													// Open copy input file
+	fastq_1_file_copy.open(fastq_1_file_name.c_str());				// Open copy input file
 
-	filter_fastq_1_file.open(filter_fastq_1_file_name.c_str());							// Open output filter file 1
-	filter_fastq_2_file.open(filter_fastq_2_file_name.c_str());							// Open output filter file 1
+	filter_fastq_1_file.open(filter_fastq_1_file_name.c_str());			// Open output filter file 1
+	filter_fastq_2_file.open(filter_fastq_2_file_name.c_str());			// Open output filter file 1
 
-	stats_file.open(stats_file_name.c_str());																// Open stats file
+	stats_file.open(stats_file_name.c_str());					// Open stats file
 
 	// Check if files can be opened properly
 	if (fastq_1_file.fail())
