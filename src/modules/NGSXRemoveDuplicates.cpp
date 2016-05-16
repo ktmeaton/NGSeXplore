3e2961ea606f7c254c7039e22369a13f0194d6f2
Katherine Eaton <ktmeaton@gmail.com>
2016-05-16 13:10:00 -0400

    Update NGSXRemoveDuplicates.cpp

--- src/modules/NGSXRemoveDuplicates.cpp
+++ src/modules/NGSXRemoveDuplicates.cpp
@@ -72,12 +72,14 @@ int main(int argc, char* argv[])
 	std::string temp_seq;
 	std::string temp_qual;
 
-	std::map<std::string, FastQ::FastQ> map_unique_fastq;					// Map to hold unique sequences
+	std::map<std::string, FastQ::FastQ> map_unique_fastq;					// Map to hold unique fastq sequences
+	std::map<std::string, FastQ::FastQ> map_unique_fasta;					// Map to hold unique fasta sequences
 
 	// Colored text and progress log
 	FastQ::FastQ temp_fastq;
-	TextColor::TextColor Palette;																						// TextColor object for coloring text output
-	ProgressLog::ProgressLog fastq_progress_log;														// ProgressLog object to store file processing progress.
+	FastQ::FastQ temp_fasta;
+	TextColor::TextColor Palette;								// TextColor object for coloring text output
+	ProgressLog::ProgressLog progress_log;							// ProgressLog object to store file processing progress.
 
 
 	int total_num_lines;																										// Number of lines in the copy fastq file
@@ -93,17 +95,17 @@ int main(int argc, char* argv[])
 	//---------------------------------Argument Parsing-----------------------------------//
 	for(int i=1;i<(argc);i++)								// Iterate through each argument (ignore first)
 	{
-		if(strcmp(argv[i],"--fastq") == 0 )
+		if(strcmp(argv[i],"--fastq") == 0 )						
 		{
-	    		fastq_file_name = std::string(argv[i+1]);
+	    		fastq_file_name = std::string(argv[i+1]);				// Store the fastq file name
 			fastqFormat=true;
 	    		i++;
 			continue;
 		}
 		
-		if(strcmp(argv[i],"--fasta") == 0 )
+		if(strcmp(argv[i],"--fasta") == 0 )					
 		{
-	    		fasta_file_name = std::string(argv[i+1]);
+	    		fasta_file_name = std::string(argv[i+1]);				// Store the fasta file name
 			fastaFormat=true;
 	    		i++;
 			continue;
@@ -111,19 +113,19 @@ int main(int argc, char* argv[])
 		
 		if(strcmp(argv[i],"--output") == 0 )
 		{
-	    		output_file_name = std::string(argv[i+1]);
+	    		output_file_name = std::string(argv[i+1]);				// Store the output file name
 	    		i++;
 			continue;
 		}	
 		
 		if(strcmp(argv[i],"--stats") == 0 )
 		{
-	    		stats_file_name = std::string(argv[i+1]);
+	    		stats_file_name = std::string(argv[i+1]);				// Store the stats file name
 	    		i++;
 			continue;
 		}	
 	
-		std::cerr<< "Unknown option " << argv[i] << " exiting." << std::endl;
+		std::cerr<< "Unknown option " << argv[i] << " exiting." << std::endl;		// Exit if unknown option
 		return 1;		
 	}
 	
@@ -137,24 +139,14 @@ int main(int argc, char* argv[])
 	}
 
 	//----------------------------------Open Files---------------------------------------//
+	
 	stats_file.open(stats_file_name.c_str());						// Generic stats file regardless of fasta/fastq input
 	output_file.open(unique_fastq_file_name.c_str());					// Generic output file regardless of fasta/fastq input
 
-	if (fastqFormat)
-	{
-		fastq_file.open(fastq_file_name.c_str());						// Open input file (open requires parameter to be a const char*)
-		file_copy.open(fastq_file_name.c_str());						// Open copy input file (open requires parameter to be a const char*)
-	}
-
 	// Check if files can be opened properly
-	if (fastq_file.fail())
+	if (output_file.fail())
 	{
-		std::cerr << "ERROR: Cannot open input fastq file: " << fastq_file_name << std::endl;
-		return 1;
-	}
-	if (unique_fastq_file.fail())
-	{
-		std::cerr << "ERROR: Cannot open unique fastq file." << unique_fastq_file_name << std::endl;
+		std::cerr << "ERROR: Cannot open output file." << output_file_name << std::endl;
 		return 1;
 	}
 	if (stats_file.fail())
@@ -163,32 +155,90 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	//----------------------------Begin Processing------------------------------//
-	std::cout << Palette.GREEN << "\nBeginning the NGSX RemoveDuplicates Module.\n" <<  Palette.RESET << std::endl;
 
-	// Count the number of sequences in the input file (using the copy)
-	std::cout << "Initializing files and counting the number of sequences (This may take a while)." << std::endl;
-	total_num_lines = std::count(std::istreambuf_iterator<char>(fastq_file_copy), std::istreambuf_iterator<char>(), '\n') + 1;
-	total_num_records = total_num_lines / 4;																// 4 lines for each sequence record
-	fastq_progress_log.initLog(total_num_records);								  				// Initialize the progres log with the total number of records
-	std::cout << "Input fastq file contains " << total_num_records << " sequences." << std::endl;
-
-
-	//---------------------------Find Unique Sequences-----------------------------------//
-
-	while (std::getline(fastq_file, current_line))
+	//----------------------------------Fastq Format------------------------------------//
+	if (fastqFormat)
 	{
-		temp_id = current_line;																										// First line is the unique sequence ID
-		std::getline(fastq_file, temp_seq);																			// Second line is the sequence bases
-		std::getline(fastq_file, current_line);																		// Third line is the "+"
-		std::getline(fastq_file, temp_qual);																		// Fourth line is the sequence quality
+		fastq_file.open(fastq_file_name.c_str());						// Open input file (open requires parameter to be a const char*)
+		file_copy.open(fastq_file_name.c_str());						// Open copy input file (open requires parameter to be a const char*)
 
-		temp_fastq.setRecord(temp_id, temp_seq, temp_qual);
-		map_unique_fastq[temp_seq] = temp_fastq;	// Add or replace sequence and FastQ object in map
+		// Check if input fastq file can be opened properly
+		if (fastq_file.fail())
+		{
+			std::cerr << "ERROR: Cannot open input fastq file: " << fastq_file_name << std::endl;
+			return 1;
+		}
+	
+		//----------------------------Begin Processing------------------------------//
+		std::cout << Palette.GREEN << "\nBeginning the NGSX RemoveDuplicates Module.\n" <<  Palette.RESET << std::endl;
 
-		// Completed reading 1 sequence record
-		fastq_progress_log.incrementLog(1);
+		// Count the number of sequences in the input file (using the copy)
+		std::cout << "Initializing files and counting the number of sequences (This may take a while)." << std::endl;
+		total_num_lines = std::count(std::istreambuf_iterator<char>(file_copy), std::istreambuf_iterator<char>(), '\n') + 1;
+		total_num_records = total_num_lines / 4;								// 4 lines for each fastq sequence record
+		progress_log.initLog(total_num_records);								// Initialize the progres log with the total number of records
+		std::cout << "Input fastq file contains " << total_num_records << " sequences." << std::endl;
+		
+		//---------------------------Find Unique Sequences-----------------------------------//
+
+		while (std::getline(fastq_file, current_line))
+		{
+			temp_id = current_line;																										// First line is the unique sequence ID
+			std::getline(fastq_file, temp_seq);						// Second line is the sequence bases
+			std::getline(fastq_file, current_line);						// Third line is the "+"
+			std::getline(fastq_file, temp_qual);						// Fourth line is the sequence quality
+
+			temp_fastq.setRecord(temp_id, temp_seq, temp_qual);
+			map_unique_fasta[temp_seq] = temp_fastq;					// Add or replace sequence and FastQ object in map
+
+			// Completed reading 1 sequence record
+			progress_log.incrementLog(1);
+		}
+
 	}
+	
+		//----------------------------------Fasta Format------------------------------------//
+	if (fastaFormat)
+	{
+		fasta_file.open(fasta_file_name.c_str());						// Open input fast file (open requires parameter to be a const char*)
+		file_copy.open(fasta_file_name.c_str());						// Open copy input fasta file (open requires parameter to be a const char*)
+
+		// Check if files can be opened properly
+		if (fasta_file.fail())
+		{
+			std::cerr << "ERROR: Cannot open input fasta file: " << fasta_file_name << std::endl;
+			return 1;
+		}
+	
+		//----------------------------Begin Processing------------------------------//
+		std::cout << Palette.GREEN << "\nBeginning the NGSX RemoveDuplicates Module.\n" <<  Palette.RESET << std::endl;
+
+		// Count the number of sequences in the input file (using the copy)
+		std::cout << "Initializing files and counting the number of sequences (This may take a while)." << std::endl;
+		total_num_lines = std::count(std::istreambuf_iterator<char>(file_copy), std::istreambuf_iterator<char>(), '\n') + 1;
+		total_num_records = total_num_lines / 2;									// 2 lines for each fasta sequence record
+		progress_log.initLog(total_num_records);								  	// Initialize the progres log with the total number of records
+		std::cout << "Input fasta file contains " << total_num_records << " sequences." << std::endl;
+
+		//---------------------------Find Unique Sequences-----------------------------------//
+
+		while (std::getline(fasta_file, current_line))
+		{
+			temp_id = current_line;								// First line is the unique sequence ID
+			std::getline(fasta_file, temp_seq);						// Second line is the sequence bases																	// Third line is the "+"
+			temp_qual = "";									// Set quality to null for fasta
+
+			temp_fasta.setRecord(temp_id, temp_seq, temp_qual);
+			map_unique_fastq[temp_seq] = temp_fasta;					// Add or replace sequence and FastQ object in map
+
+			// Completed reading 1 sequence record
+			progress_log.incrementLog(1);
+		}
+
+	}
+
+
+
 
 
 	//---------------------------Write Unique Sequences-----------------------------------//
