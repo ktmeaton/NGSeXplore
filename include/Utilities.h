34a48f7a52e1663691980b0fa2b35e978b5c6668
Katherine Eaton <ktmeaton@gmail.com>
2017-07-14 15:26:11 -0400

    Began function IntersectMaps implementation

--- include/Utilities.h
+++ include/Utilities.h
@@ -8,8 +8,6 @@ namespace Utilities
 	template <typename T> std::string to_string( const T& n);
 
 	template < typename KeyType, typename ValueType>
-	map<KeyType, ValueType>
-	IntersectMaps(const map<KeyType, ValueType>& m1,
+	map<KeyType, ValueType> IntersectMaps(const map<KeyType, ValueType>& m1,
 								const map<KeyType, ValueType>& m2);
-
 }
