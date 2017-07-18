f251e3c6c8d2f1d39984a2093ba390c66951a6f2
Katherine Eaton <ktmeaton@gmail.com>
2017-07-18 14:05:25 -0400

    Remove merge strings

--- include/Utilities.h
+++ include/Utilities.h
@@ -7,14 +7,8 @@ namespace Utilities
 {
 
     template <typename T> std::string to_string( const T& n);
-
-
-<<<<<<< HEAD
+    
     template<typename KeyType, typename ValueType> std::map<KeyType,std::pair<ValueType, ValueType> > IntersectMaps(const std::map<KeyType, ValueType>& m1, const std::map<KeyType, ValueType>& m2);
-=======
-    template<typename KeyType, typename ValueType, typename BF> std::map<KeyType,ValueType> IntersectMaps(const std::map<KeyType, ValueType>& m1, const std::map<KeyType, ValueType>& m2, BF bf);
->>>>>>> 65f2833463a68eec6ace181f266c1bc22ae88c5d
-
 }
 
 #include "Utilities.cpp"
